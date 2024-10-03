e435800d37463d62361e52602882cbd3710cdefe
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-10-03 15:17:49 +0700

    Module8: final

---
+++ Module8/Cleaner.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    static class Cleaner
+    {   
+        /// <summary>
+        /// Метод для первого задания - чистит указанную директорию от файлов и папок, которые не использовались более 30 минут
+        /// </summary>
+        /// <param name="URL"></param>
+        public static void CleanUp(string URL)
+        {
+            const int mins = 30;
+            int foldsCount = 0;
+            int filesCount = 0;
+            try
+            {
+                if (Directory.Exists(URL))
+                {
+                    var dir = new DirectoryInfo(URL);
+                    foreach (FileInfo file in dir.GetFiles())
+                    {
+                        DateTime dt = file.LastAccessTime; // в задании речь шла про "использование", думаю, LastAccessTime подойдёт
+                        if (TimeSpan.FromMinutes(mins) <= (DateTime.Now - dt))
+                        {
+                            file.Delete();
+                            filesCount++;
+                        }
+                    }
+
+                    foreach (DirectoryInfo fold in dir.GetDirectories())
+                    {
+                        DateTime dt = fold.LastAccessTime;
+                        if (TimeSpan.FromMinutes(mins) <= (DateTime.Now - dt))
+                        {
+                            fold.Delete(true);
+                            foldsCount++;
+                        }
+                    }
+                    Console.WriteLine($"Unused for {mins} minutes directories and files has been deleted");
+                    Console.WriteLine($"Totally removed {foldsCount + filesCount} objects: {foldsCount} folders and {filesCount} files");
+                }
+                else Console.WriteLine("Directory does not exists");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Error: {err}");
+            }
+        }
+
+        /// <summary>
+        /// Метод для второго задания - считает в байтах объём файлов в директории, а также файлов во вложенных в директорию папках
+        /// </summary>
+        /// <param name="URL"></param>
+        public static float VolumeCounter(string URL)
+        {
+            float Vol = 0;
+            try
+            {
+                if (Directory.Exists(URL))
+                {
+                    var dir = new DirectoryInfo(URL);
+                    foreach (FileInfo file in dir.GetFiles())
+                    {
+                        Vol += file.Length;
+                    }
+
+                    foreach (DirectoryInfo fold in dir.GetDirectories())
+                    {
+                        Vol += VolumeCounter(fold.FullName);
+                    }
+                }
+                else Console.WriteLine("Directory does not exists");
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Error: {err}");
+            }
+            return Vol;
+        }
+
+        /// <summary>
+        /// Корректный на мой взгляд метод по очистке - удаляет только устаревшие файлы (в т.ч. и вложенные), 
+        /// а папки удаляет только в случае, если они становятся пустыми после чистки
+        /// </summary>
+        /// <param name="URL"></param>
+        public static (int f, int d) CleanUpCorrect(string URL)
+        {
+            const int mins = 30;
+            int foldsCount = 0;
+            int filesCount = 0;
+            try
+            {
+                if (Directory.Exists(URL))
+                {
+                    var dir = new DirectoryInfo(URL);
+                    foreach (FileInfo file in dir.GetFiles())
+                    {
+                        DateTime dt = file.LastAccessTime; // в задании речь шла про "использование", думаю, LastAccessTime подойдёт
+                        if (TimeSpan.FromMinutes(mins) <= (DateTime.Now - dt))
+                        {
+                            file.Delete();
+                            filesCount++;
+                        }
+                    }
+
+                    foreach (DirectoryInfo fold in dir.GetDirectories())
+                    {
+                        (int f, int d) = CleanUpCorrect(fold.FullName); // нужно, чтобы рекурсия учитывала вложенные удалённые файлы
+                        foldsCount += d;
+                        filesCount += f;
+                        DateTime dt = fold.LastAccessTime;
+                        if (TimeSpan.FromMinutes(mins) <= (DateTime.Now - dt))
+                        {
+                            try
+                            {
+                                fold.Delete();
+                                foldsCount++;
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                    }
+                    //Console.WriteLine($"Unused for {mins} minutes directories and files has been deleted");
+                    //Console.WriteLine($"Totally removed {foldsCount + filesCount} objects: {foldsCount} folders and {filesCount} files");
+                }
+                else Console.WriteLine("Directory does not exists");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Error: {err}");
+            }
+            return (filesCount, foldsCount);
+        }
+
+        /// <summary>
+        /// Метод для третьего задания - объединяет в себе два предыдущих метода, один их которых переработан для корректного выполнения
+        /// </summary>
+        /// <param name="URL"></param>
+        public static void Combined(string URL)
+        {
+            /* Console.WriteLine($"Directory's volume before cleanup: {VolumeCounter(URL)}");
+            CleanUp(URL);
+            Console.WriteLine($"Directory's volume after cleanup: {VolumeCounter(URL)}");
+            
+            Хотел, было, сказать, зачем плодить методы, если можно использовать комбинацию уже готовых решений,
+            но столкнулся с тем, что если в папке изменить файл, то время обращения к папке всё равно не изменится и она будет удалена в соответствии с алгоритмом первого метода
+            Поэтому переделал первый метод так, чтобы он удалял только "несвежие" файлы (в т.ч. и вложенные), а папки удалял только если они оказались пустые в результате чистки*/
+
+            Console.WriteLine($"Directory's volume before cleanup: {VolumeCounter(URL)}");
+
+            (int f, int d) = CleanUpCorrect(URL);
+            Console.WriteLine($"Totally removed {d + f} objects: {d} folders and {f} files");
+
+            Console.WriteLine($"Directory's volume after cleanup: {VolumeCounter(URL)}");
+
+        }
+    } 
+}
--- Module8/Program.cs
+++ Module8/Program.cs
@@ -1,135 +1,77 @@
-﻿//using System.IO;
-//using System.Linq;
-
-using System.IO.Enumeration;
-
+﻿
 namespace Module8
 {
     internal class Program
     {
-        const string FileName = @"C:\Users\slavi\Desktop\BinaryFile.bin";
+        const string FileName = @"C:\Users\User\Desktop\students.dat"; // данные для 4 задания
+        const string Destination = @"C:\Users\User\Desktop\Students"; // данные для 4 задания
         static void Main(string[] args)
         {
-            // -----------информация о дисках-----------
-            //DriveInfo[] drivers = DriveInfo.GetDrives();
+            Cleaner.CleanUp(@"C:\test1" /* вставить путь к директории */); // метод для первого задания
 
-            //foreach (DriveInfo driver in drivers.Where(d => d.DriveType == DriveType.Fixed)) // можно выбрать тип дисков, о которых хотим посмотреть информацию (почему-то работает только с fixed)
-            //{
-            //    Console.WriteLine();
-            //    Console.WriteLine("Name: " + driver.Name);
-            //    Console.WriteLine("Type: " + driver.DriveType);
+            Console.WriteLine(Cleaner.VolumeCounter(@"C:\test2" /* вставить путь к директории */)); // метод для второго задания
 
-            //    if (driver.IsReady)
-            //    {
-            //        Console.WriteLine("Format: " + driver.DriveFormat);
-            //        Console.WriteLine("Total size: " + driver.TotalSize);
-            //        Console.WriteLine("Total free space: " + driver.TotalFreeSpace);
-            //        Console.WriteLine("Avalible free space: " + driver.AvailableFreeSpace);
-            //        Console.WriteLine("Label: " + driver.VolumeLabel);
-            //    }
-            //    Console.WriteLine();
-            //}
-            // -----------информация о дисках-----------
+            Cleaner.Combined(@"C:\test3" /* вставить путь к директории */); // метод для третьего задания
 
-            // -----------создание и считывание файла-----------
-            //string path = @"C:\Users\slavi\source\repos\Module8\Module8\Program.cs";
-            //if (File.Exists(path))
-            //{
-            //    using (StreamWriter sw = File.AppendText(path))
-            //    {
-            //        sw.WriteLine($"// {DateTime.Now}"); // доабвление даты работы с файлом
-            //    }
-            //}
 
-            //using (StreamReader sr = File.OpenText(path))
-            //{
-            //    string line = "";
-            //    while ((line = sr.ReadLine()) != null)
-            //    {
-            //        Console.WriteLine(line);
-            //    }
-            //}
-            // -----------создание и считывание файла-----------
+            List<Student> studs = Reader(FileName);
 
-            //-----------информация о каталогах диска-----------
-            //Catalogs();
-            // -----------информация о каталогах диска-----------
 
-            // -----------работа с бинарными файлами-----------
-            //WriteValues();
-            //ReadValues();
-            // -----------работа с бинарными файлами-----------
+            if (!Directory.Exists(Destination))
+            {
+                Directory.CreateDirectory(Destination);
+            }
 
-            Cleaner.CleanUp(@"C:\Users\Slavi\Desktop\test");
+            foreach (Student student in studs)
+            {
+                string filename = $"{Destination}\\{student.Group}.txt";
+                
+                FileInfo f = new FileInfo(filename);
+
+                if (!f.Exists)
+                {
+                    using (StreamWriter sw = f.CreateText())
+                    {
+                        sw.WriteLine(string.Concat(student.Name, ", ", student.DateOfBirth, ", ", student.AverageScore));
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = f.AppendText()) // если запусить код ещё раз, а файлы уже будут созданы и будут содержать данные об учениках, информация просто продублируется - не придумал, как это побороть
+                    {
+                        sw.WriteLine(string.Concat(student.Name, ", ", student.DateOfBirth, ", ", student.AverageScore));
+                    }
+                }
+            }
         }
-        /// <summary>
-        /// Метод для получения информации о каталогах диска
-        /// </summary>
-        //static void Catalogs()
-        //{
-        //    string dir = @"C:\";
-        //    if (Directory.Exists(dir))
-        //    {
-        //        Console.WriteLine("Folders: ");
-        //        string[] folds = Directory.GetDirectories(dir);
-        //        string[] files = Directory.GetFiles(dir);
 
-        //        foreach (string fold in folds)
-        //        {
-        //            Console.WriteLine(fold);
-        //        }
+        static List<Student> Reader(string f)
+        {
+            List<Student> result = new();
+            using FileStream fs = new FileStream(f, FileMode.Open);
+            using StreamReader sr = new StreamReader(fs);
 
-        //        Console.WriteLine();
-        //        Console.WriteLine("Files: ");
+            //Console.WriteLine(sr.ReadToEnd()); - зочемъ?
 
-        //        foreach (string file in files)
-        //        {
-        //            Console.WriteLine(file);
-        //        }
+            fs.Position = 0;
 
-        //        Console.WriteLine(folds.Length + files.Length);
-        //    }
-        //}
+            BinaryReader br = new BinaryReader(fs);
 
+            while (fs.Position < fs.Length)
+            {
+                Student student = new Student();
+                student.Name = br.ReadString();
+                student.Group = br.ReadString();
+                long dt = br.ReadInt64();
+                student.DateOfBirth = DateTime.FromBinary(dt);
+                student.AverageScore = br.ReadDecimal();
 
-        /// <summary>
-        /// Запись бинарного файла
-        /// </summary>
-        //static void WriteValues()
-        //{
-        //    using (BinaryWriter writer = new BinaryWriter(File.Open(FileName, FileMode.Create)))
-        //    {
-        //        writer.Write($"File created {DateTime.Now} on {Environment.OSVersion}");
-        //        //writer.Write("Text");
-        //        //writer.Write(35);
-        //        //writer.Write(false);
-        //        //writer.Write(true);
-        //    }
-        //}
-        /// <summary>
-        /// Чтение бинарного файла
-        /// </summary>
-        //static void ReadValues()
-        //{
-        //    //float FloatVal;
-        //    string StringVal;
-        //    //int IntVal;
-        //    //bool BoolVal;
-            
-        //    if (File.Exists(FileName))
-        //    {
-        //        using (BinaryReader reader = new BinaryReader(File.Open(FileName, FileMode.Open)))
-        //        {
-        //            //FloatVal = reader.ReadSingle();
-        //            StringVal = reader.ReadString();
-        //            //IntVal = reader.ReadInt32();
-        //            //BoolVal = reader.ReadBoolean();
-        //        }
+                result.Add(student);
+            }
 
-        //        Console.WriteLine(string.Concat(StringVal));
-        //    }
-        //}
+            fs.Close();
 
+            return result;
+        }
     }
 }
-// 02.10.2024 13:01:40
